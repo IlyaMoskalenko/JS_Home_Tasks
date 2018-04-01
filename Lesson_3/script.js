906c7638f8fa967462d0c9bfb52f155e453e0e7b
Ilya Moskalenko <ilyamoskalenko75@gmail.com>
2018-04-01 16:53:38 +0300

    JS_Home_Task_4

--- Lesson_3/script.js
+++ Lesson_3/script.js
@@ -1,113 +1,111 @@
-var N = Number(prompt("Введите N:")); 
-var M = Number(prompt("Введите M:")); 
+// var N = Number(prompt("Введите N:")); 
+// var M = Number(prompt("Введите M:")); 
 
-var Names = new Array(N); 
-var Cities = new Array(M); 
+// var Names = new Array(N); 
+// var Cities = new Array(M); 
 
-Names.fill(0); 
-Cities.fill(0); 
+// Names.fill(0); 
+// Cities.fill(0); 
 
-Names = Names.map(function(){ 
-return "someName" + (Math.round(Math.random()*2*N)).toString();; 
+// Names = Names.map(function(){ 
+// return "someName" + (Math.round(Math.random()*2*N)).toString();; 
+// }); 
+
+// Cities = Cities.map(function(){ 
+// return "someCity" + (Math.round(Math.random()*2*M)).toString();; 
+// }) 
+
+// var func = function(){ 
+// console.log(this.name,"-", this.city, "-", this.age); 
+// // console.log("City: ", this.city); 
+// // console.log("Age: ", this.age); 
+// console.log("\n"); 
+// } 
+
+// var People = []; 
+
+// for (var i = 0; i < N; i++){ 
+// var newMan = new Object(); 
+// newMan.name = Names[Math.round(Math.random()*(N-1))]; 
+// newMan.age = Math.round(Math.random()*50+10); 
+// newMan.city = Cities[Math.round(Math.random()*(M-1))]; 
+// newMan.getInfo = func; 
+// People.push(newMan); 
+// } 
+
+// console.log("Unsorted:");
+// console.log("Name   -", "   city   -", "   age"); 
+// for (var i = 0; i < N; i++){ 
+// People[i].getInfo(); 
+// }
+
+// People = People.sort(function(a,b){
+// 			return a.age < b.age;
+// 		});
+
+// console.log("Sorted:");
+// console.log("Name   -", "   city   -", "   age");   
+// for (var i = 0; i < N; i++){ 
+// People[i].getInfo(); 
+// }
+
+// задание 2
+
+var nW = Number(prompt("Введите количество работников:")); 
+var mW = Math.round(Math.random()*2*nW)
+var Names1 = new Array(nW); 
+var Departments = new Array(mW);
+
+Names1.fill(0); 
+Departments.fill(0); 
+
+Names1 = Names1.map(function(){ 
+return "workerName" + (Math.round(Math.random()*10*nW)).toString();; 
 }); 
 
-Cities = Cities.map(function(){ 
-return "someCity" + (Math.round(Math.random()*2*M)).toString();; 
+Departments = Departments.map(function(){ 
+return "Department" + (Math.round(Math.random()*10*mW)).toString();; 
 }) 
 
-var func = function(){ 
-console.log(this.name,"-", this.city, "-", this.age); 
-// console.log("City: ", this.city); 
-// console.log("Age: ", this.age); 
-console.log("\n"); 
+function func1(){ 
+	console.log("Name: ",this.name); 
+	console.log("Age: ", this.age); 
+	console.log("Department: ", this.department); 
+	console.log("Salary: ", this.salary);
+	console.log("Experience: ", this.exp);
+	console.log("\n"); 
 } 
 
-var People = []; 
-
-for (var i = 0; i < N; i++){ 
-var newMan = new Object(); 
-newMan.name = Names[Math.round(Math.random()*(N-1))]; 
-newMan.age = Math.round(Math.random()*50+10); 
-newMan.city = Cities[Math.round(Math.random()*(M-1))]; 
-newMan.getInfo = func; 
-People.push(newMan); 
-} 
-
-console.log("Unsorted:");
-console.log("Name   -", "   city   -", "   age"); 
-for (var i = 0; i < N; i++){ 
-People[i].getInfo(); 
+function func2(arg,arr){
+	if(typeof(arg)=="string"){
+		for(var i = 0; i < nW; i++){
+			if (arr[i].name == arg){
+				arr.splice(i,1);
+				window.nW--;
+			}
+		}
+	}
 }
 
-People = People.sort(function(a,b){
-			return a.age < b.age;
-		});
+var buh = {};
 
-console.log("Sorted:");
-console.log("Name   -", "   city   -", "   age");   
-for (var i = 0; i < N; i++){ 
-People[i].getInfo(); 
-}
+buh.workers = [];
 
+	for (var i = 0; i < nW; i++){ 
+		var newWorker = new Object(); 
+		newWorker.name = Names1[Math.round(Math.random()*(nW-1))]; 
+		newWorker.age = Math.round(Math.random()*30+18); 
+		newWorker.department = Departments[Math.round(Math.random()*(mW-1))]; 
+		newWorker.salary = Math.round(Math.random()*2000+500); 
+		newWorker.exp = Math.round(Math.random()*20);
+		newWorker.Info = func1; 
+		buh.workers.push(newWorker); 
+	}
+console.log(buh.workers); 
 
-
-// спросить почему не работает 
-
-// var N = Number(prompt("Введите N:"));
-// var M = Number(prompt("Введите M:"));
-
-// var Names = new Array(N);
-// var Cities = new Array(M);
-
-// Names.fill(0);
-// Cities.fill(0);
-
-// Names = Names.map(function(){
-// 		return "someName" + (Math.round(Math.random()*N)).toString();;
-// 	});
-
-// Cities = Cities.map(function(){
-// 		return "someCity" + (Math.round(Math.random()*M)).toString();;
-// 	})
-
-// //console.log(Names);
-// //console.log(Cities);
-
-// var man = {
-// 		name: "",
-// 		age: 0,
-// 		city: ""	
-// 	}
-
-// var People = new Array(N);
-// People.fill(0);
-
-// People.forEach(function(item,i,People){
-// 	console.log("i =",i);
-// 	var newMan = new Object(man);
-// 	newMan.name = Names[Math.round(Math.random()*(N-1))];
-// 	newMan.age = Math.round(Math.random()*50+10);
-// 	newMan.city = Cities[Math.round(Math.random()*(M-1))];
-// 	newMan.getInfo =  function(){
-// 			console.log("Name: ", this.name);
-// 			console.log("City: ", this.city);
-// 			console.log("Age: ", this.age);
-// 			console.log("\n")
-// 		}
-// 	People[i] = newMan;
-// 	People[i].getInfo();
-// });
-
-
-
-// //console.log(People);
-
-// People.forEach(function(item,i,People){
-// 	console.log("i =",i);
-// 	People[i].getInfo();
-// });
-
-// // People = People.sort(function(a,b){
-// // 		return a.age < b.age;
-// // })
-
+buh.addOrDelete = func2;
+// for (var i = 0; i < nW; i++){ 
+// buh.workers[i].Info(); 
+// }
+buh.addOrDelete(buh.workers[0].name,buh.workers);
+console.log(buh.workers); 
---
+++ Lesson_4/script.js
@@ -0,0 +1,50 @@
+//Задание 1
+
+var getInfo = function(){
+	var Cookies = (navigator.cookieEnabled).toString();
+	var AppName = (navigator.appName).toString();
+	var AppCodeName = (navigator.appCodeName).toString();
+	var Platform = (navigator.platform).toString();
+	var Language = (navigator.language).toString();
+	var BrowserEngine = (navigator.product).toString();
+	var AppVersion = (navigator.appVersion).toString();
+
+	console.log("AppName: ", AppName);
+	console.log("AppCodeName: ", AppCodeName);
+	console.log("Platform: ", Platform)
+	console.log("Cookies: ", Cookies);
+	console.log("Language: ", Language);
+	console.log("BrowserEngine: ", BrowserEngine);
+	console.log("AppVersion: ", AppVersion);
+}
+
+window.getInfo();
+console.log('');
+
+//Задание 2
+
+str1 = "375295553535";
+str2 = "375(29)5553535";
+str3 = "375 29 555-35-35";
+str4 = "375 29 5553535";
+str5 = "+375(29)555-35-35";
+str6 = "555-55-555-55-55"
+
+str5_1 = "+375 (29) 555-35-35";    
+str5_2 = "+375-(29)-555-35-35";
+str5_3 = "+375  29  555-35-35";
+
+var r = /\+?\d{3}((\-\d{2}\-)|(\s\d{2}\s)|(\(\d{2}\))|(\d{2}))\d{3}[(\s)|(\-)]?\d{2}[(\s)|(\-)]?\d{2}/;
+
+console.log(str1.match(r));
+console.log(str2.match(r));
+console.log(str3.match(r));
+console.log(str4.match(r));
+console.log(str5.match(r));
+console.log(str6.match(r));
+
+console.log(str5_1.match(r));
+console.log(str5_2.match(r));
+console.log(str5_3.match(r));
+
+con
